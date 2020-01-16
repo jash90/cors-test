0412a923d3810422558794184ceb8145e6c64cb3
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2020-01-16 21:03:19 +0100

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="greet">
+      <H1>Test</H1>
+    </button>
+
+    <H1></H1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      msg: "Hello"
+    };
+  },
+  methods: {
+    greet() {
+      fetch("http://smarthome.raccoonsoftware.pl/conditions")
+        .then(response => response.json())
+        .then(responseJson => {
+          msg = JSON.stringify(responseJson);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
